54c3b551dc7a27ed62741e39afb09cd04fd7f6f4
Adam Freidin <adam.freidin@gmail.com>
2011-02-18 18:38:46 -0800

    "uses" framework. dialog feature moved into a partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def uses feat
+    @features ||= {}
+	return unless not @features.has_key?(feat.to_sym)
+    @features[feat] = true
+    print(capture do  
+      render(:partial => feat.to_s)
+    end)
+  end
 end
